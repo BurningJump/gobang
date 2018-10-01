a39657a7986b831861ac361ab5c497d4c3d51fcc
BurningJump <935646729@qq.com>
2018-10-01 11:14:08 +0800

    完成五子棋算法

--- gobang.js
+++ gobang.js
@@ -1,11 +1,72 @@
 var chessBoard = [];
 var me = true;
+var gameover = false;
+
+// 赢法数组
+var wins = [];
+
+// 赢法的统计数组
+var myWin = [];
+var computerWin = [];
 
 for (var i = 0; i < 15; i++) {
   chessBoard[i] = [];
   for (var j = 0; j < 15; j++) {
     chessBoard[i][j] = 0;
   }
+}
+
+for (var i = 0; i < 15; i++) {
+  wins[i] = [];
+  for (var j = 0; j < 15; j++) {
+    wins[i][j] = [];
+  }
+}
+
+// 第count种赢法
+var count = 0;
+
+// 所有横线
+for (var i = 0; i < 15; i++) {
+  for (var j = 0; j < 11; j++) {
+    for (var k = 0; k < 5; k++) {
+      wins[i][j + k][count] = true;
+    }
+    count++;
+  }
+}
+// 所有竖线
+for (var i = 0; i < 15; i++) {
+  for (var j = 0; j < 11; j++) {
+    for (var k = 0; k < 5; k++) {
+      wins[j + k][i][count] = true;
+    }
+    count++;
+  }
+}
+// 所有斜线
+for (var i = 0; i < 11; i++) {
+  for (var j = 0; j < 11; j++) {
+    for (var k = 0; k < 5; k++) {
+      wins[i + k][j + k][count] = true;
+    }
+    count++;
+  }
+}
+// 所有反斜线
+for (var i = 0; i < 11; i++) {
+  for (var j = 14; j > 3; j--) {
+    for (var k = 0; k < 5; k++) {
+      wins[i + k][j - k][count] = true;
+    }
+    count++;
+  }
+}
+console.log(count);
+
+for (var i = 0; i < count; i++) {
+  myWin[i] = 0;
+  computerWin[i] = 0;
 }
 
 var chess = document.getElementById('chess');
@@ -50,17 +111,107 @@ var oneStep = function(i, j, me) {
 }
 
 chess.onclick = function(e) {
+  if (gameover) return;
+  if (!me) return;
   var x = e.offsetX;
   var y = e.offsetY;
   var i = Math.floor(x / 30);
   var j = Math.floor(y / 30);
   if (chessBoard[i][j] === 0) {
     oneStep(i, j, me);
-    if (me) {
-      chessBoard[i][j] = 1;
-    } else {
-      chessBoard[i][j] = 2;
+    chessBoard[i][j] = 1;
+    for(var k = 0; k < count; k++) {
+      if (wins[i][j][k]) {
+        myWin[k]++;
+        computerWin[k] = 6;
+        if(myWin[k] === 5) {
+          window.alert('你赢了！');
+          gameover = true;
+        }
+      }
     }
+    if (!gameover) {
+      me = !me;
+      computerAI();
+    }
+  }
+}
+
+var computerAI = function() {
+  var myScore = [];
+  var computerScore = [];
+  var max = 0;
+  var u = 0, v = 0;
+  for (var i = 0; i < 15; i++) {
+    myScore[i] = [];
+    computerScore[i] = [];
+    for (var j = 0; j < 15; j++) {
+      myScore[i][j] = 0;
+      computerScore[i][j] = 0;
+    }
+  }
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 15; j++) {
+      if (chessBoard[i][j] === 0) {
+        for (var k = 0; k < count; k++) {
+          if (wins[i][j][k]) {
+            if (myWin[k] === 1) {
+              myScore[i][j] += 200;
+            } else if (myWin[k] === 2) {
+              myScore[i][j] += 400;
+            } else if (myWin[k] === 3) {
+              myScore[i][j] += 2000;
+            } else if (myWin[k] === 4) {
+              myScore[i][j] += 10000;
+            }
+
+            if (computerWin[k] === 1) {
+              computerScore[i][j] += 220;
+            } else if (computerWin[k] === 2) {
+              computerScore[i][j] += 420;
+            } else if (computerWin[k] === 3) {
+              computerScore[i][j] += 2100;
+            } else if (computerWin[k] === 4) {
+              computerScore[i][j] += 20000;
+            }
+          }
+        }
+        if (myScore[i][j] > max) {
+          max = myScore[i][j];
+          u = i;
+          v = j;
+        } else if (myScore[i][j] === max) {
+          if (computerScore[i][j] > computerScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+        if (computerScore[i][j] > max) {
+          max = computerScore[i][j];
+          u = i;
+          v = j;
+        } else if (computerScore[i][j] === max) {
+          if (myScore[i][j] > myScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+      }
+    }
+  }
+  oneStep(u, v, false);
+  chessBoard[u][v] = 2;
+  for (var k = 0; k < count; k++) {
+    if (wins[u][v][k]) {
+      computerWin[k]++;
+      myWin[k] = 6;
+      if (computerWin[k] === 5) {
+        window.alert('你输了！');
+        gameover = true;
+      }
+    }
+  }
+  if (!gameover) {
     me = !me;
   }
 }
